25d456272ac8f40e81686947765a3f6ba4f3da76
DanielLutz97 <daniel_lutz97@hotmail.com>
2024-07-30 08:56:52 -0300

    Special move - Promotion

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,11 +1,9 @@
 package application;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import chess.ChessMatch;
